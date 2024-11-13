cbd7153130c86ba31f365dd6a47f09677fa95660
Brayann Benavides <brayannbegu11@gmail.com>
2024-11-13 10:15:15 -0500

    Operations working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::stdin;
-
 use regex::Regex;
 
 fn main() {
@@ -7,8 +5,10 @@ fn main() {
 
     // Regex
 
-    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-    let re_prod = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let re_add = Regex::new(r"(\d+)\s*\+\s*(\d+)").unwrap();
+    let re_mult = Regex::new(r"(\d+)\s*\*\s*(\d+)").unwrap();
+    let re_div = Regex::new(r"(\d+)\s*\/\s*(\d+)").unwrap();
+    let re_subs = Regex::new(r"(\d+)\s*\-\s*(\d+)").unwrap();
     // (\d+)\s?\+\s?(\d+) -> 21 + 23
 
     // User Data
@@ -19,7 +19,7 @@ fn main() {
 
     //Product
     loop {
-        let caps = re_prod.captures(expression.as_str());
+        let caps = re_mult.captures(expression.as_str());
 
         if caps.is_none() {
             break;
@@ -29,10 +29,27 @@ fn main() {
         let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
         let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
 
-        let product: i32 = left_value * right_value;
+        let multiplication: i32 = left_value * right_value;
 
-        expression = expression.replace(cap_expression, &product.to_string());
+        expression = expression.replace(cap_expression, &multiplication.to_string());
     }
+    // Division
+    loop {
+        let caps = re_div.captures(expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+        let cap_expression: &str = caps.get(0).unwrap().as_str();
+        let left_value: f64 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: f64 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let division: f64 = left_value / right_value;
+
+        expression = expression.replace(cap_expression, &division.to_string());
+    }
+
     //Addition
     loop {
         let caps = re_add.captures(expression.as_str());
@@ -49,8 +66,22 @@ fn main() {
 
         expression = expression.replace(cap_expression, &addition.to_string());
     }
+    // Substraction
+    loop {
+        let caps = re_subs.captures(expression.as_str());
 
-    // Operations
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+        let cap_expression: &str = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let substraction: i32 = left_value - right_value;
+
+        expression = expression.replace(cap_expression, &substraction.to_string());
+    }
 
     // Show result
     println!("Result: {}", expression)
